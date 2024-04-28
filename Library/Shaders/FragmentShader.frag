3f89f0635500dd4493fe5b03d5aa73bbd50b9df6
Jlthepig <levitanj@sabje.co.za>
2024-04-28 10:24:52 +0200

    Trying to overlay a texture

--- Library/Shaders/FragmentShader.frag
+++ Library/Shaders/FragmentShader.frag
@@ -9,5 +9,5 @@ uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = texture(texture1, TexCoord), texture(texture2,TexCoord)  *vec4(ourColor,1.0);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2,TexCoord),0.3)  ;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -131,15 +131,15 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    const char* imgpath2 = "Library/Images/kosmos-grafika-risunok-zvezdy-planety.jpg";
+    const char* imgpath2 = "Library/Images/Greenlaser.png";
 
     unsigned char* data2 = stbi_load(imgpath2, &width, &height, &nrChannels, 0);
     if (data2)
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
         std::cout << "image successfully loaded " << imgpath2;
     }
